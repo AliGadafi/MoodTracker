aabedd0be403e10c4313a334a7b91b069072e684
Ali Gadafi <aligadafi2004@gmail.com>
2025-04-15 23:37:02 -0400

    Added buttons

--- MoodTracker/ContentView.swift
+++ MoodTracker/ContentView.swift
@@ -20,6 +20,38 @@ struct ContentView: View {
                     }
                     .navigationTitle("Mood Tracker")
                 }
+        
+        HStack(spacing: 20) {
+            Button(action: {
+                selectedMood = "Happy"
+            }) {
+                Text("😊 Happy")
+                    .padding()
+                    .background(selectedMood == "Happy" ? Color.green : Color.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
+            Button(action: {
+                selectedMood = "Meh"
+            }) {
+                Text("😐 Meh")
+                    .padding()
+                    .background(selectedMood == "Meh" ? Color.green : Color.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
+            Button(action: {
+                selectedMood = "Sad"
+            }) {
+                Text("😢 Sad")
+                    .padding()
+                    .background(selectedMood == "Sad" ? Color.green : Color.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
     }
 }
 
