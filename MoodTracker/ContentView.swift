e5d2f94c4a8937525a0576433c842eeba3576dc4
Ali Gadafi <aligadafi2004@gmail.com>
2025-04-15 23:35:45 -0400

    Start NavigationView

--- MoodTracker/ContentView.swift
+++ MoodTracker/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedMood: String = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+                    VStack(spacing: 30) {
+                        Text("How are you feeling today?")
+                            .font(.title)
+                            .padding(.top, 40)
+                    }
+                    .navigationTitle("Mood Tracker")
+                }
     }
 }
 
