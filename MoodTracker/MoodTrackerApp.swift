8ee629a9f68ea61379a15075303e0f30c6d69e90
Ali Gadafi <aligadafi2004@gmail.com>
2025-04-15 23:31:27 -0400

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Ali Gadafi on 2025-04-15.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodTrackerTests/MoodTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerTests.swift
+//  MoodTrackerTests
+//
+//  Created by Ali Gadafi on 2025-04-15.
+//
+
+import Testing
+@testable import MoodTracker
+
+struct MoodTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
